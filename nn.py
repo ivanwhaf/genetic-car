884a117db4e3d901b21d9d25a85896fd231ace07
Ivan <ivanwyf@foxmail.com>
2020-07-31 19:27:49 +0800

    modify something!

--- nn.py
+++ nn.py
@@ -4,57 +4,58 @@ import numpy as np
 
 def sigmoid(x):
     # sigmoid function: f(x) = 1 / (1 + e^(-x))
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
 
 def tanh(x):
     # tanh function: f(x) = (e^(x) - e^(-x)) / (e^(x) + e^(-x))
-    return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
 
 def deriv_sigmoid(x):
-    # derivative of simgoid function: f'(x) = f(x) * (1 - f(x))
+    # derivative of sigmoid function: f'(x) = f(x) * (1 - f(x))
     fx = sigmoid(x)
-    return fx*(1-fx)
+    return fx * (1 - fx)
 
 
 def mse_loss(y_true, y_predict):
     # loss function
-    return ((y_true-y_predict)**2).mean()
+    return ((y_true - y_predict) ** 2).mean()
 
 
-def get_random_weights(size)->list:
-    ret=[]
+def get_random_weights(size) -> list:
+    ret = []
     for i in range(size):
-        ret.append(random.uniform(-1,1))
+        ret.append(random.uniform(-1, 1))
     return ret
 
 
 def get_random_bias():
-    return random.uniform(-1,1)
+    return random.uniform(-1, 1)
 
 
 class Neuron:
     """
     single neuron class
     """
+
     def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
 
-
-    def feedforward(self, inputs,activation='sigmoid'):
-        '''
+    def feedforward(self, inputs, activation='sigmoid'):
+        """
         output=0
         for i in range(len(inputs)):
             output+=inputs[i]*weights[i]
-        '''
-        output = np.dot(self.weights, inputs)+self.bias
-        if activation=='sigmoid':
+        """
+        output = np.dot(self.weights, inputs) + self.bias
+        if activation == 'sigmoid':
             return sigmoid(output)
-        elif activation=='tanh':
+        elif activation == 'tanh':
             return tanh(output)
-        elif activation=='no':
+        elif activation == 'no':
             return output
-
 
 
 class NeuralNetwork:
@@ -104,74 +105,18 @@ class NeuralNetwork:
 
         output_h = [output_h1, output_h2, output_h3, output_h4, output_h5]
 
-        output_o1 = self.os[0].feedforward(output_h,'tanh')
+        output_o1 = self.os[0].feedforward(output_h, 'tanh')
         output_o2 = self.os[1].feedforward(output_h)
 
         return [output_o1, output_o2]
-
-    def train(self, x_train, y_train):
-        '''
-        epochs=1000  # train loop times
-        lr=0.1  # learning rate
-        for epoch in range(epochs):
-            for x, y_true in zip(x_train,y_train):
-                w1=self.h1.weights[0]
-                w2=self.h1.weights[1]
-                w3=self.h1.weights[2]
-                w4=self.h2.weights[0]
-                w5=self.h2.weights[1]
-                w6=self.h2.weights[2]
-                w7=self.h3.weights[0]
-                w8=self.h3.weights[1]
-                w9=self.h3.weights[2]
-                w10=self.h4.weights[0]
-                w11=self.h4.weights[1]
-                w12=self.h4.weights[2]
-                w13=self.h5.weights[0]
-                w14=self.h5.weights[1]
-                w15=self.h5.weights[2]
-                b1=self.h1.bias
-                b2=self.h2.bias
-                b3=self.h3.bias
-                b4=self.h4.bias
-                b5=self.h5.bias
-
-                w16=self.o1.weights[0]
-                w17=self.o1.weights[1]
-                w18=self.o1.weights[2]
-                w19=self.o1.weights[3]
-                w20=self.o1.weights[4]
-                w21=self.o2.weights[0]
-                w22=self.o2.weights[1]
-                w23=self.o2.weights[2]
-                w24=self.o2.weights[3]
-                w25=self.o2.weights[4]
-                b6=self.o1.bias
-                b7=self.o2.bias
-
-                sum_h1=w1*x[0] + w2*x[1] + w3*x[2] + b1
-                h1=sigmoid(sum_h1)
-
-                sum_h2=w4*x[0] + w5*x[1] + w6*x[2] + b2
-                h2=sigmoid(sum_h2)
-
-                sum_h3=w7*x[0] + w8*x[1] + w9*x[2] + b3
-                h3=sigmoid(sum_h3)
-
-                sum_h4=w10*x[0] + w11*x[1] + w12*x[2] + b4
-                h4=sigmoid(sum_h4)
-
-                sum_h5=w13*x[0] + w14*x[1] + w15*x[2] + b5
-                h5=sigmoid(sum_h5)
-        '''
 
 
 def sort_score(nets) -> list:
     """
     sort networks by their score(fitness), big-->small
     """
-    for i in range(len(nets)-1):
-        for j in range(i+1, len(nets)):
+    for i in range(len(nets) - 1):
+        for j in range(i + 1, len(nets)):
             if nets[i].score < nets[j].score:
                 # swap
                 temp = nets[i]
@@ -190,13 +135,13 @@ def update_score(nets) -> list:
     return nets
 
 
-def roulette_selection(nets,ratio) -> NeuralNetwork:
+def roulette_selection(nets, ratio) -> NeuralNetwork:
     """
     select one network from networks' mating pool by using 'roullette wheel selection' algorithm
     'roulette' algorithm tend to select one network that has higher score
     """
-    nets=[nets[i] for i in range(int(len(nets)*ratio))]
-    # calculate all networks' toatal score
+    nets = [nets[i] for i in range(int(len(nets) * ratio))]
+    # calculate all networks' total score
     total_score = 0
     for net in nets:
         total_score += net.score
@@ -204,7 +149,7 @@ def roulette_selection(nets,ratio) -> NeuralNetwork:
     # calculate every network's accum_rate
     accum_rate = 0
     for i in range(len(nets)):
-        rate = nets[i].score/total_score  # one's probability
+        rate = nets[i].score / total_score  # one's probability
         accum_rate += rate
         nets[i].accum_rate = accum_rate
 
@@ -215,21 +160,21 @@ def roulette_selection(nets,ratio) -> NeuralNetwork:
             return net
 
 
-def get_elites(nets,ratio) ->list:
+def get_elites(nets, ratio) -> list:
     """
     get top 1/4 elites networks from this generation
     """
-    elites = [nets[i] for i in range(int(len(nets)*ratio))]
+    elites = [nets[i] for i in range(int(len(nets) * ratio))]
     return elites
 
 
-def crossover(nets,ratio) -> NeuralNetwork:
+def crossover(nets, ratio) -> NeuralNetwork:
     """
     select two networks through 'roullette wheel selection' algorithm,
     then cross their genes(weights) randomly to get a new child(network)
     """
-    father = roulette_selection(nets,ratio)
-    mother = roulette_selection(nets,ratio)
+    father = roulette_selection(nets, ratio)
+    mother = roulette_selection(nets, ratio)
     child = NeuralNetwork()
     # crossover hidden layer
     for i in range(len(child.hs)):
@@ -244,11 +189,11 @@ def crossover(nets,ratio) -> NeuralNetwork:
             child.os[i] = father.os[i]
         else:
             child.os[i] = mother.os[i]
-    
+
     return child
 
 
-def mutation(nets,pm,range) -> list:
+def mutation(nets, pm, range_) -> list:
     """
     mutate next generation networks by 'Pm'
     'Pm' is the probability of mutation
@@ -259,8 +204,8 @@ def mutation(nets,pm,range) -> list:
             if random.random() < pm:
                 # mutate weights and bias
                 for k in range(len(nets[i].hs[j].weights)):
-                    nets[i].hs[j].weights[k] += random.uniform(-range, range)*nets[i].hs[j].weights[k]
-                nets[i].hs[j].bias += random.uniform(-range, range)*nets[i].hs[j].bias
+                    nets[i].hs[j].weights[k] += random.uniform(-range_, range_) * nets[i].hs[j].weights[k]
+                nets[i].hs[j].bias += random.uniform(-range_, range_) * nets[i].hs[j].bias
     return nets
 
 
@@ -275,7 +220,7 @@ def main():
     inputs = [2, 3, 4]
     print(network.feedforward(inputs))
 
-    # networks list, every item is a NuralNetwork
+    # networks list, every item is a NeuralNetwork
     nets = []
 
     # initialize networks list, size: 100
@@ -289,25 +234,24 @@ def main():
     nets = sort_score(nets)
 
     # top 1/4 elite networks
-    elites=get_elites(nets,0.25)
+    elites = get_elites(nets, 0.25)
 
     # next generation's networks list
-    next_gen_nets=[]
+    next_gen_nets = []
 
     # add this generation's elites directly to next generation
     next_gen_nets.extend(elites)
 
-    # create hybrid children and add them to next geration until enough
-    for i in range(int(len(nets)/4*3)):
-        child = crossover(nets,0.3)
+    # create hybrid children and add them to next generation until enough
+    for i in range(int(len(nets) / 4 * 3)):
+        child = crossover(nets, 0.3)
         next_gen_nets.append(child)
 
     # mutate next generation's every network including elites
-    next_gen_nets=mutation(next_gen_nets,0.1,0.1)
+    next_gen_nets = mutation(next_gen_nets, 0.1, 0.1)
 
     print([net.hs[1].bias for net in next_gen_nets])
 
 
 if __name__ == "__main__":
     main()
-
